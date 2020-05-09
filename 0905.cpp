a3d95e56c60fbb81402b0cb9115adb8a6993cd7f
SCP120 <64856050+SCP120@users.noreply.github.com>
2020-05-09 21:09:04 +0700

    0905

---
+++ 0905.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main(){
+	int a;
+	printf("nhap ngay: ");
+	scanf("%d",&a);
+	if(a>=2 && a<=7){
+		printf("day la thu %d",a);
+		}else if(a=8){ printf("day la chu nhat");
+	}else{ printf("day khong phai ngay trong tuan");
+	}
+}
